03565261612f3f4eb06e4f4f84afe6aed80523a7
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-15 02:53:53 -0700

    Added two column layout to listings

--- ledgerlockerfrontend/src/App.js
+++ ledgerlockerfrontend/src/App.js
@@ -15,150 +15,174 @@ let addr = "";
 let box;
 
 const listings = [
-  {
-    name: "House 1",
+	{
+		name: "House 1",
 
-    address: "123 Broadway, Denver, CO",
-    rent: "50 DAI",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Isaac_Bell_House_2018-06-13.jpg/1200px-Isaac_Bell_House_2018-06-13.jpg"
-  },
-  {
-    name: "House 2",
-    address: "253 11th St, Denver, CO",
-    rent: "100 DAI",
-    img:
-      "https://www.trbimg.com/img-577423d8/turbine/ct-elite-street-sixteen-candles-evanston-home-for-sale-0630-biz-20160629"
-  }
+		address: "123 Broadway, Denver, CO",
+		rent: "50 DAI",
+		img:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Isaac_Bell_House_2018-06-13.jpg/1200px-Isaac_Bell_House_2018-06-13.jpg"
+	},
+	{
+		name: "House 2",
+		address: "253 11th St, Denver, CO",
+		rent: "100 DAI",
+		img:
+			"https://www.trbimg.com/img-577423d8/turbine/ct-elite-street-sixteen-candles-evanston-home-for-sale-0630-biz-20160629"
+	}
 ];
 
 async function handleGetAccounts() {
-  web3.eth.getAccounts().then(async address => {
-    addr = address[0];
-    console.log("Address: ", addr);
-    await open3Box();
-    await set3BoxData("testing", "value");
-    await get3BoxData("testing");
-    await remove3BoxData("testing");
-  });
+	web3.eth.getAccounts().then(async address => {
+		addr = address[0];
+		console.log("Address: ", addr);
+		await open3Box();
+		await set3BoxData("testing", "value");
+		await get3BoxData("testing");
+		await remove3BoxData("testing");
+	});
 }
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
+	return (
+		<Router>
+			<div>
+				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/listingdetails">
-            <ListingDetails />
-          </Route>
-          <Route path="/lockcontrols">
-            <LockControls />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/listings">
-            <Listings />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+				<Switch>
+					<Route path="/listingdetails">
+						<ListingDetails />
+					</Route>
+					<Route path="/lockcontrols">
+						<LockControls />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+					<Route path="/listings">
+						<Listings />
+					</Route>
+					<Route path="/profile">
+						<Profile />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 function Home() {
-  return (
-    <div>
-      <nav
-        className="navbar is-light"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          ></img>
-        </a>
-        <Link className="navbar-item" to="/">
-          Home
-        </Link>
-        <Link className="navbar-item" to="/search">
-          Search
-        </Link>
-        <div class="navbar-end">
-          <Link className="navbar-item" to="/profile">
-            <img src={profileImage} className="profile-img" />
-          </Link>
-        </div>
-      </nav>
-      <button onClick={() => handleGetAccounts()}>
-        Get Accounts + 3Box Testing
-      </button>
-      {listings.map((l, i) => (
-        <div key={i}>
-          <h1>{l.name}</h1>;
-          <p>
-            Address: <b>{l.address}</b>
-            Rent: <b>{l.rent}</b>
-            <img src={l.img}></img>
-          </p>
-        </div>
-      ))}
-    </div>
-  );
+	const leftColumnListings = [];
+	const rightColumnListings = [];
+
+	for (let i = 0; i < listings.length; i += 2) {
+		leftColumnListings.push(listings[i]);
+		rightColumnListings.push(listings[i + 1]);
+	}
+
+	return (
+		<div>
+			<nav
+				className="navbar is-light"
+				role="navigation"
+				aria-label="main navigation"
+			>
+				<a className="navbar-item" href="https://bulma.io">
+					<img
+						src="https://bulma.io/images/bulma-logo.png"
+						width="112"
+						height="28"
+					></img>
+				</a>
+				<Link className="navbar-item" to="/">
+					Home
+				</Link>
+				<Link className="navbar-item" to="/search">
+					Search
+				</Link>
+				<div class="navbar-end">
+					<Link className="navbar-item" to="/profile">
+						<img src={profileImage} className="profile-img" />
+					</Link>
+				</div>
+			</nav>
+			<button onClick={() => handleGetAccounts()}>
+				Get Accounts + 3Box Testing
+			</button>
+			<div className="columns">
+				<div className="column">
+					{leftColumnListings.map((l, i) => (
+						<div key={i}>
+							<h1>{l.name}</h1>;
+							<p>
+								Address: <b>{l.address}</b>
+								Rent: <b>{l.rent}</b>
+								<img src={l.img}></img>
+							</p>
+						</div>
+					))}
+				</div>
+        <div className="column">
+					{rightColumnListings.map((l, i) => (
+						<div key={i}>
+							<h1>{l.name}</h1>;
+							<p>
+								Address: <b>{l.address}</b>
+								Rent: <b>{l.rent}</b>
+								<img src={l.img}></img>
+							</p>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 function Listings() {
-  return <p>testing</p>;
+	return <p>testing</p>;
 }
 
 function Profile() {
-  return <p>testing 2</p>;
+	return <p>testing 2</p>;
 }
 
 function ListingDetails() {
-  // Includes listing confirmation modal
-  return (
-    <section>
-      <h1>LISTING DETAILS</h1>
-    </section>
-  );
+	// Includes listing confirmation modal
+	return (
+		<section>
+			<h1>LISTING DETAILS</h1>
+		</section>
+	);
 }
 
 function LockControls() {
-  return (
-    <section>
-      <h1>LOCK CONTROLS:</h1>
-      <button>Unlock</button>
-      <button>Lock</button>
-    </section>
-  );
+	return (
+		<section>
+			<h1>LOCK CONTROLS:</h1>
+			<button>Unlock</button>
+			<button>Lock</button>
+		</section>
+	);
 }
 
 async function open3Box() {
-  box = await Box.openBox(addr, fm.getProvider());
-  await box.syncDone;
-  console.log("Opened 3box box for ", addr);
+	box = await Box.openBox(addr, fm.getProvider());
+	await box.syncDone;
+	console.log("Opened 3box box for ", addr);
 }
 
 async function set3BoxData(key, value) {
-  await box.private.set(key, value);
-  console.log("Set private value in 3box (key: ", key, ")");
+	await box.private.set(key, value);
+	console.log("Set private value in 3box (key: ", key, ")");
 }
 
 async function get3BoxData(key) {
-  await box.private.get(key);
-  console.log("Got private value in 3box (key: ", key, ")");
+	await box.private.get(key);
+	console.log("Got private value in 3box (key: ", key, ")");
 }
 
 async function remove3BoxData(key) {
-  await box.private.remove(key);
-  console.log("Removed private value in 3box (key: ", key, ")");
+	await box.private.remove(key);
+	console.log("Removed private value in 3box (key: ", key, ")");
 }

b46c95b499708655358fb2dd83b1467109ab68d0
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-15 12:54:22 -0700

    Added fetching from API

--- ledgerlockerfrontend/src/components/MainListing/MainListing.js
+++ ledgerlockerfrontend/src/components/MainListing/MainListing.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./MainListing.css"
 
 export default function MainListing(props) {
 	return (
-		<div key={props.key}>
-			<h1>{props.listing.name}</h1>;
+		<div className="box" key={props.key}>
+			<h1>{props.listing.name}</h1>
 			<p>
 				Address: <b>{props.listing.address}</b>
 				Rent: <b>{props.listing.rent}</b>
-				<img src={props.listing.img}></img>
+				<img className="listing-img" src={props.listing.img}></img>
 			</p>
 		</div>
 	);

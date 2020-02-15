ea01c16aac5f4a3028c8b733882516ded3e1c5db
Jason Du <contact@dujason.com>
2020-02-15 04:38:53 -0500

    Refactor DAI lending contract

--- eth/contracts/DAILending.sol
+++ eth/contracts/DAILending.sol
@@ -3,20 +3,7 @@ pragma solidity ^0.5.0;
 import "./Account.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Interface for Compound's CEther contract
-// Minting and redeeming CEther
-contract CEther {
-    function mint() external payable;
-    function redeem(uint redeemTokens) external returns (uint);
-    function exchangeRateCurrent() public returns (uint);
-    function balanceOfUnderlying(address account) public returns (uint);
-    function redeemUnderlying(uint redeemAmount) public returns (uint);
-    // Part of the ERC20 Token Standard
-    function balanceOf(address owner) external view returns (uint256 balance);
-}
-
 // This contract manages interactions to current lending protocols
-// TODO: Extend to CDai and DSR 
 contract DAILending is Account {
     
     IERC20 daiContract = IERC20(address(0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e));
@@ -27,14 +14,20 @@ contract DAILending is Account {
         _redeemUnderlying(totalBalance);
         
         // Send DAI to User
-        msg.sender.transfer(totalBalance);
+        require(daiContract.transfer(msg.sender, totalBalance), "Something went wrong");
     }
     
     function _transfer(address _to, uint256 _value, uint256 _duration) internal {
         super._transfer(_to, _value, _duration);
+        
+        // Transfer DAI to contract
+        require(daiContract.transferFrom(msg.sender, address(this), _value), "Something went wrong");
+        
+        // Handle transfer of DAI
+        _handleInterestToken(_value);
     }
     
-    function _mintCToken() internal {
+    function _handleInterestToken(uint256 _balance) internal {
         // Put the paid amount into compound here
         // cToken.mint.value(msg.value)();
     }

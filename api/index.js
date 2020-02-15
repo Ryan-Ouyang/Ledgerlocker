e27e61fda1ef802e2be215decfeaa5f176ced8a8
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-15 00:40:58 -0700

    Added socket to communicate with lock

--- api/index.js
+++ api/index.js
@@ -2,7 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+var io =  require('socket.io')(http);
+
 app.use(cors());
 app.use(express.json());
+
+io.on('connection', function(socket){
+    console.log("LOCK CONNECTED")
+})
+
+app.post('/api/unlock/', function(req, res) {
+    io.sockets.emit('open sesame', req);
+})
+
+app.post('/api/lock/', function(req, res) {
+    io.sockets.emit('lock', req);
+})
+
+app.post('api/changeID', function(req, res) {
+    io.sockets.emit('change', req);
+})
 
 app.listen(3001, () => console.log("API listening on port 3001"));

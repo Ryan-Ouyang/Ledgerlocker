56910b68b70722804ca591ef32f35cf4a4c0b814
Haardik <hhaardik@edu.uwaterloo.ca>
2020-02-15 04:57:28 -0500

    api

--- ledgerlockerfrontend/src/App.js
+++ ledgerlockerfrontend/src/App.js
@@ -14,24 +14,6 @@ let web3 = new Web3(fm.getProvider());
 let addr = "";
 let box;
 
-const listings = [
-  {
-    name: "House 1",
-
-    address: "123 Broadway, Denver, CO",
-    rent: "50 DAI",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Isaac_Bell_House_2018-06-13.jpg/1200px-Isaac_Bell_House_2018-06-13.jpg"
-  },
-  {
-    name: "House 2",
-    address: "253 11th St, Denver, CO",
-    rent: "100 DAI",
-    img:
-      "https://www.trbimg.com/img-577423d8/turbine/ct-elite-street-sixteen-candles-evanston-home-for-sale-0630-biz-20160629"
-  }
-];
-
 async function handleGetAccounts() {
   web3.eth.getAccounts().then(async address => {
     addr = address[0];
